437eb01e170309a2b5c1214e6f39f877b12076b8
Asonglefac Millenium Nzefe <asonglefacmillenium@gmail.com>
2020-08-28 10:58:22 +0100

    blog page template

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,60 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+
+    <div>
+    <div>
+<img src="../assets/background.png" alt="" class="img-fluid w-100" >
+    </div>
+
+    <div class="head">
+   <h1>BLOG:</h1>
+   <p>Read our blog posts to know more about our platform and blood donation related news and campaigns</p>
+    </div>
+    </div>
+
+<div class="container shadow post">
+  <b-row>
+    <b-col>
+      <img src="../assets/benefit1.png" alt=""  width="400px" height="400px">
+    </b-col>
+    <b-col>
+      <h4>Healthy habits for blood donors</h4>
+
+      <p>In as much as blood donation is very necessary, the health of the donor is also of top importance. There are certain healthy habits that the blood donor must endevor to follow such as sports, good feeding habits, regular checkups etc...</p>
+      <button><a href="">Read the post<i class="fa fa-arrow-right" aria-hidden="true"></i></a></button>
+    </b-col>
+  </b-row>
+</div>
+
+<div class="container shadow post">
+  <b-row>
+    <b-col>
+      <img src="../assets/benefit2.png" alt=""  width="400px" height="400px">
+    </b-col>
+    <b-col>
+      <h4>Benefits of Blood donation</h4>
+
+      <p>We are so many times made to understand that the positive effects of blood donation always are beneficial only to the recipient. But this is not all that true as even the donor has alot of things to benefit healthwise via this process...</p>
+      <button><a href="">Read the post<i class="fa fa-arrow-right" aria-hidden="true"></i></a></button>
+    </b-col>
+  </b-row>
+</div>
+
+<div class="container shadow  post">
+  <b-row>
+    <b-col>
+      <img src="../assets/benefit3.png" alt=""  width="400px" height="400px">
+    </b-col>
+    <b-col>
+      <h4>Types of blood groups</h4>
+
+      <p>In this noble act that saves the lives of many over the globe, it is very important to handle matters related to it with care. This is why the ABO blood system must be highly considered before blood donation...</p>
+
+ <button><a href="">Read the post<i class="fa fa-arrow-right" aria-hidden="true"></i></a></button>
+    </b-col>
+  </b-row>
+</div>
+ </div>
 </template>
 
 <script>
@@ -53,6 +80,39 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: black;
+  background-color: crimson;
+}
+
+button{
+  background-color: crimson;
+  color: black;
+  margin-top: 80px;
+}
+
+.head{
+  position: absolute;
+  top: 20%;
+  left: 30%;
+  text-align: center;
+  color: white;
+  font-size: 20px;
+}
+
+.post{
+  margin-top: 40px;
+  margin-bottom: 40px;
+}
+
+.post img{
+  margin-left: 0px;
+}
+
+.post h4{
+  margin: 40px;
+  color: crimson;
+}
+.post p{
+  margin-top: 60px;
 }
 </style>
